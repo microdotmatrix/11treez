bcd52c40eff42a2127894d5a1df283c33322c91f
microdotmatrix <liquid.knives.2@gmail.com>
2022-05-19 08:51:19 -0500

    1232

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import {$,jQuery} from 'jquery';
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
+
+//import luge from '@waaark/luge'
+import { gsap, ScrollTrigger, Flip } from "gsap/all";
+
